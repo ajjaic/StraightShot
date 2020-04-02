1301b5bedc360d861f32698deb01194e709bf4e0
AJ <ajjaic@cseries.us>
2020-04-02 10:11:29 -0400

    massive refactoring organizing assets. Need to figure out how to use LFS

---
+++ Assets/_game/_Common/Music/DontDestroyMusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DontDestroyMusic : MonoBehaviour
+{
+    private void Start()
+    {
+        var objs = GameObject.FindGameObjectsWithTag("GameMusic");
+
+        if (objs.Length > 1)
+            Destroy(gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+    }
+}
